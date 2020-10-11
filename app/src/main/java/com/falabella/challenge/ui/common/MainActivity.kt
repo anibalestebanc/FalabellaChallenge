b8d9efa9051a2e0075f81f7669c017cf453f4614
Anibal Cortez <anibal.cortez.arenas@everis.com>
2020-10-11 00:23:42 -0300

    Setup app bar with navigation component.

--- app/src/main/java/com/falabella/challenge/ui/common/MainActivity.kt
+++ app/src/main/java/com/falabella/challenge/ui/common/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.falabella.challenge.ui.common
 
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.falabella.challenge.R
-import com.falabella.challenge.ui.common.BaseActivity
 
 class MainActivity : BaseActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 
 }
